fed663507ff6b1add2a7e17bee43be04802be31a
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-07-28 00:05:51 -0300

    Condicionais, laços de repeticao, date

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-      //Dicionario
+ //Dicionario
   
-  var frutas = [{nome:"maça",cor:"vermelho"},{nome:'uva',cor:'roxa'}];
+  /*var frutas = [{nome:"maça",cor:"vermelho"},{nome:'uva',cor:'roxa'}];
   console.log(frutas);
-  alert(frutas[1].nome)
+  alert(frutas[1].nome)*/
 
      //array
 //var lista = ["maça","pera","laranja"];
@@ -16,4 +16,55 @@ lista.pop(3);*/
 /*var nome = "victor weiland";
 var idade = 21;
 
-alert(nome + " tem " + idade + " anos");*/
+alert(nome + " tem " + idade + " anos");
+*/
+
+
+
+      //condicionais
+     /*
+     var idade = 18;
+      if(idade >= 18){
+        alert('maior de idade');
+      }else{
+        alert('menor de idade');
+      };    
+     */ 
+      
+    //Prompt - exibi caixa de dialogo do navegador
+    /** 
+      var idade = prompt ('qual sua idade? ');
+     */
+      
+      /**
+       * Laços de Repetição
+       * 
+       * var count = 5;
+       * while (count <= 5){
+       *  console.log(count);
+       *  count ++
+       * };
+       * 
+       *   ou
+       * 
+       * var count;
+       * for(count=0; count <=5; count++){
+       *    console.log(count);
+       * };
+       * 
+       */
+
+      /**
+       * Date - declarar datas
+       * 
+       * var d = new Date();
+       * alert(d.getMonth()+1); - mes
+       * alert(d.getDay()); - dia
+       * 
+       * alert(d.getMinutes()); - minutos
+       * alert(d.getHours()); - horas
+       * 
+       */
+      
+      
+     
