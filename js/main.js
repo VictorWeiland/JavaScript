f984fe6a43254b0194bcffec7385f372dfc8ee0e
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-07-27 23:38:15 -0300

    add array e dicionario

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-var nome = "victor weiland";
+      //Dicionario
+  
+  var frutas = [{nome:"maça",cor:"vermelho"},{nome:'uva',cor:'roxa'}];
+  console.log(frutas);
+  alert(frutas[1].nome)
+
+     //array
+//var lista = ["maça","pera","laranja"];
+/*lista.push("uva");
+lista.pop(3);*/
+//console.log(lista.length);
+//console.log(lista.reverse);
+//console.log(lista.toString());
+//console.log(lista.join(""));
+
+/*var nome = "victor weiland";
 var idade = 21;
 
-alert(nome + " tem " + idade + " anos");
+alert(nome + " tem " + idade + " anos");*/
